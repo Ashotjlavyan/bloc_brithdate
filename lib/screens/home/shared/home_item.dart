78706678f01d3ef435962b63cca9781b7912f01f
Ashot Jlavyan <ashot.jlavyan@gmail.com>
2021-10-11 12:46:55 +0400

    FIX home items ui issues

--- lib/screens/home/shared/home_item.dart
+++ lib/screens/home/shared/home_item.dart
@@ -12,12 +12,45 @@ class HomeItem extends StatelessWidget {
             borderRadius: BorderRadius.all(Radius.circular(16.0))),
         height: 150,
         child: Row(children: [
-          Expanded(child: Container()),
+          Expanded(
+              child: Container(
+                  margin: const EdgeInsets.only(left: 10), child: const _Content())),
           Container(
               width: 50,
               color: Colors.transparent,
               child: const _ArrowButton()),
         ]));
+  }
+}
+
+class _Content extends StatelessWidget {
+  const _Content({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          const Align(
+              alignment: Alignment.topLeft,
+              child: Text('Track my period',
+                  textAlign: TextAlign.start,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20,
+                      fontWeight: FontWeight.w800))),
+          Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                  margin: const EdgeInsets.only(top: 10),
+                  child: const Text('Contraception and welldbering',
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 18,
+                      )))),
+        ]);
   }
 }
 
@@ -28,9 +61,17 @@ class _ArrowButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: 20,
+      height: 20,
       margin: const EdgeInsets.only(right: 20.0),
-      decoration:
-          const BoxDecoration(color: Colors.blue, shape: BoxShape.circle),
+      decoration: const BoxDecoration(
+          color: ColorConstant.arrowColor, shape: BoxShape.circle),
+      child: Container(
+          margin: const EdgeInsets.all(4),
+          decoration: const BoxDecoration(
+              image: DecorationImage(
+            image: AssetImage('assets/images/arrow.png'),
+            fit: BoxFit.contain,
+          ))),
     );
   }
 }
--- lib/shared/constant.dart
+++ lib/shared/constant.dart
@@ -2,4 +2,7 @@ import 'package:flutter/material.dart';
 
 class ColorConstant {
   static const Color buttonColor = Color(0xfffaeeee);
+  static const Color arrowColor = Color(0xff555587);
+
+  
 }
